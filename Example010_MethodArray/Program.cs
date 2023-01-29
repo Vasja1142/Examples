fd8bfed1836fef7cf42cc6e260d194247bd7f2cf
Vasiliy Gordeev <vasja11420@gmail.com>
2023-01-29 16:44:05 +0500

    Example010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {11, 12, 23, 53, 45, 64, 74, 45, 25};
+
+int n = array.Length;
+int find = 45;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
