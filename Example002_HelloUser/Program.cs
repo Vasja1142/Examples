89a64b75b5819bfa3743bba4dd6f2d5c15ff958c
Vasya comp <vasja11420@gmail.com>
2023-02-14 13:51:16 +0500

    изменения

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,3 +1,3 @@
 ﻿Console.Write("Введите ваше имя ");
-string username = Console.ReadLine();
+string username = Console.ReadLine()!;
 Console.Write($"Привет, {username}");
---
+++ Example013_RecursionAlgoritm/Program.cs
@@ -0,0 +1,65 @@
+﻿Console.Clear();
+// string[,] table = new string[2, 5];
+// //String.Empty
+// table[1, 2] = "слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int colums = 0; colums < 5; colums++)
+//     {
+//         Console.WriteLine($"-{table[rows, colums]}-");
+//     }
+// }
+
+// int[, ] matrix = new int[3, 4];
+
+
+// void PrintArray(int[,] matr)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($"{matrix[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+//      Console.WriteLine();
+// }
+
+
+// void FillArray(int[,] matr)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matr[i,j] = new Random().Next(1,10);
+//         }
+//     }
+// }
+
+// PrintArray(matrix);
+// FillArray(matrix);
+// PrintArray(matrix);
+
+// int Factorial(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+
+// Console.WriteLine(Factorial(3));
+
+double Fibonacci(int n)
+{
+    if (n == 0) return 1;
+    else 
+        if (n == 1) return 1;
+        else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 0; i < 40; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
