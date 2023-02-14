c63a1e10883020e0df3d596f1fbcf16684f0c88d
Vasya comp <vasja11420@gmail.com>
2023-02-14 18:09:00 +0500

    Исправление ошибок

--- Example013_RecursionAlgoritm/Program.cs
+++ Example013_RecursionAlgoritm/Program.cs
@@ -11,37 +11,37 @@
 //     }
 // }
 
-// int[, ] matrix = new int[3, 4];
+int[, ] matrix = new int[3, 4];
 
 
-// void PrintArray(int[,] matr)
-// {
-//     for (int i = 0; i < matrix.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matrix.GetLength(1); j++)
-//         {
-//             Console.Write($"{matrix[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-//      Console.WriteLine();
-// }
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+     Console.WriteLine();
+}
 
 
-// void FillArray(int[,] matr)
-// {
-//     for (int i = 0; i < matrix.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matrix.GetLength(1); j++)
-//         {
-//             matr[i,j] = new Random().Next(1,10);
-//         }
-//     }
-// }
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
 
-// PrintArray(matrix);
-// FillArray(matrix);
-// PrintArray(matrix);
+PrintArray(matrix);
+FillArray(matrix);
+PrintArray(matrix);
 
 // int Factorial(int n)
 // {
@@ -51,15 +51,15 @@
 
 // Console.WriteLine(Factorial(3));
 
-double Fibonacci(int n)
-{
-    if (n == 0) return 1;
-    else 
-        if (n == 1) return 1;
-        else return Fibonacci(n - 1) + Fibonacci(n - 2);
-}
+// double Fibonacci(int n)
+// {
+//     if (n == 0) return 1;
+//     else 
+//         if (n == 1) return 1;
+//         else return Fibonacci(n - 1) + Fibonacci(n - 2);
+// }
 
-for (int i = 0; i < 40; i++)
-{
-    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
-}
+// for (int i = 0; i < 40; i++)
+// {
+//     Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+// }
